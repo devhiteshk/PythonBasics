c1de5706dee828a20c171d0e5fe4986927f14cc3
Hitesh Kumar <hitesh.k.83080@gmail.com>
2021-02-28 13:56:51 +0530

    Add files via upload

---
+++ Week9P5FriInterviewPrep.py
@@ -0,0 +1,328 @@
+# If you’re looking for a new career or job as a Python developer, then all these lessons would be for naught if you
+# can’t pass the interview process. For this Friday, we’re going to cover the process of a general software
+# development interview. We’ll cover each stage, what to do before and after the interview, whiteboarding,
+# answering general and technical questions, and how to contour your resumes and profiles. This lesson is meant to be
+# helpful for those either struggling on the interview process or those of you who have never had a formal software
+# development interview. If you have no interest in this section, and wish to continue, use today as a break from
+# this book’s schedule. Developer Interview Process The interview process for a developer role can be broken down
+# into many different stages. In the following, you’ll find the main stages that many companies in the industry
+# practice. Keep in mind that this is a general interview process and not every company will follow these to a tee.
+# Use this section as more of a guide on what to possibly expect: • Stage 1 – Basic questions about yourself along
+# with past work experience. The first step will usually be a phone call with a 3rd party recruiter,
+# internal recruiter, HR, or talent acquisition of the company. During the first step of the interview process,
+# the interviewer is trying to gauge if you are the correct fit for the role. They are looking for you to mention the
+# “Buzzwords” along with providing information on why you are a good fit for the position. You want to relate
+# yourself to the position. Be sure to talk about your experience using the languages and technologies they’re
+# looking for. The interviewer is looking for you to meet half of the requirements to make yourself a good match. No
+# one will ever know everything, but it is good to show them what you know and your willingness to learn.
+
+
+# Note Buzzwords are keywords that the position is looking for. For example, a
+# back-end position using Python would expect to hear words like API, JSON, Python,
+# Flask, Django, Jinja, Relational Databases, PostgreSQL, etc.
+
+
+# Stage 2 – If you’ve made it past the phone screen, you’ll usually be asked to come in for an in-person interview.
+# This stage is generally where you meet other developers that currently work at the company. Although they’ll ask
+# you interview questions, this stage is generally for the employees to see if they would like to work with you and
+# get to know you on a more personal level. Generally, you’ll interview with small groups of employees at a time.
+# You’ll have about two to five of these sessions that will last around 10–15 minutes each. Before hiring an
+# individual, these groups will generally get together to discuss potential candidates for the next stage. During
+# this stage, be sure to properly introduce yourself and shake each person’s hand. Get to know each employee,
+# and try to relate with them on a personal level.
+
+
+# Stage 3 – This is the technical round. In this stage, questions will be asked
+# to assess the developer’s skills and abilities. Generally, there will
+# be a whiteboard question, a couple technical questions on
+# paper, and a brain teaser. This stage is generally conducted with
+# the hiring manager, or team manager that you’ll be working with.
+# When asked a question, make sure you understand it clearly. You
+# are more than welcome to ask as many questions as you need to
+# clearly understand the problem before answering the question. If
+# you do not know the answer to the question, let the interviewer
+# know that you have not worked with that concept or do not see
+# the problem. The interviewer during this stage will know if you
+# have no idea what you’re talking about so don’t try and make
+# something up. They’ll be more impressed with your honesty and
+# try to guide you through the problem. During this stage, they
+# don’t care if you’re right or wrong. They’re more interested in how
+# you think and how well you can problem-solve.
+
+
+# Stage 4
+# – At this point, you’re generally sitting with the hiring manager or
+# an HR personnel. In this stage, you can ask questions about the
+# company, as well as the job role. If you’ve made it this far, the
+# company has seen value in you as a potential employee. Usually,
+# this is where contract negotiations and salary conversations
+# occur. At the end of the interview, always have questions ready to
+# ask and lots of them. If you have no questions, it’s generally a sign
+# of not being prepared or laziness.
+
+
+# What to Do Before the Interview
+# In almost everything that you do in life, you can never be too prepared. The same goes
+# for interviewing. The following are tips for what you should do before your interview:
+# • Research
+# – Be sure to research the company you’re interviewing for. Don’t
+# just understand what products they create, or services they offer,
+# but know what charities they support, the companies they partner
+# with, etc. It shows that you’re involved and care about the companies’ well-being. A little goes a long way.
+# • Be Prepared
+# – Put together a folder or portfolio of that includes your resume, a
+# pad of paper for taking notes during the interview, examples of
+# work, etc.
+
+
+# Resume
+# – Always print on resume on higher quality paper.
+# – Contour your resume to the job you’re interviewing for. For
+# example, for back-end roles, mention Python, SQL, database�related technologies, etc.
+# – Keep your resume to a single page.
+# – Don’t add any fluff.
+# – Keep it organized with sections like experience, skills, and
+# education.
+# – Think of your resume as a 30-second elevator pitch.
+# – Often, it helps to have a designer overlook your resume. Some
+# sites will do this for a small fee but help to make your resume look
+# more professional and organized.
+
+
+# Portfolio Web Site – Not all developers have personal web sites, but it certainly looks bad when you don’t. Imagine
+# going to a dentist that has no teeth. View yourself as the product that you’re trying to sell to companies,
+# you should have a web site that shows your skills and allows others to contact you.
+
+
+# Github
+# – Almost every hiring agency and company will look to your Github
+# to see the projects you’ve worked on.
+# – It’s best to have complete projects on your portfolio as well. One
+# major project will always stand out better than 10 minor projects.
+# – Include your Github account in your resume, portfolio web site,
+# and e-mails.
+
+
+# LinkedIn
+# – Most recruiters and companies are on LinkedIn for one reason,
+# and that’s to look for potential candidates for a job posting.
+# Make sure your profile is up to date with all relative information
+# and projects that you’ve worked on.
+# – Your profile picture should be professional. You don’t need to be
+# in a suit and tie, but it’s best not to have a picture of you on a
+# beach.
+# – Look at this web site as your professional networking service.
+# – Post often with information from the field you want to work in.
+# The more you post, the more apt a recruiter is to recognize you.
+
+
+# Social Media
+# – Make it private or keep it clean. You better believe companies will
+# look at your posts for a way to understand who you are, and if they
+# don’t like what they see, you won’t be getting a call back.
+
+
+# Apply Directly – It always looks more professional to send in an application directly to the company. Often,
+# you’ll find a job you like on Indeed or ZipRecruiter; however, these companies get flooded with applications every
+# day on these sites, and they generally have algorithms to eliminate most candidates. Sending a direct e-mail shows
+# that you put time and effort into directly contacting the company.
+
+
+# General Questions
+# The following is a list of general nontechnical questions, followed by an example of
+# a good answer. These questions were selected because they are usually asked and
+# answered improperly:
+# • What salary are you looking for?
+# – “I don’t have an exact number right now. I’d like to do some more
+# research on what other companies are offering for a similar
+# position. What do you pay your employees on average for this
+# position?”
+# – Never state a number when they ask, this provides leverage for
+# them during any negotiation process.
+
+
+# – Counter their question with another question.
+# – If they continue to ask you for a number, simply state the same
+# response.
+# • Where do you see yourself in five years?
+# – “I’m more so focused on my skills over the next five years. I know
+# that focusing on continuing my education and improvement of
+# myself will lead me to where I need to be.”
+# – Focusing on improving your skills shows compassion.
+# • Why did you want to be a software developer?
+# – “I’ve always been intrigued by being able to build something out
+# of nothing, and I’ve always enjoyed a challenge. When you’re able
+# to solve problems and build applications, it’s a wonderful feeling.”
+# – Show the passion that you have as a developer; it will always
+# come off as a strength.
+# – Never mention it’s about money, even if it is.
+# • Why are you changing careers?
+# – “It felt like I wasn’t being challenged enough in my previous
+# career and I’ve always been interested in programming and the
+# thrill that comes with building applications that improve people’s
+# lives.”
+# – Like the previous question, show the passion and drive that you
+# have for this career.
+# – Explaining that you like to be challenged shows your not lazy.
+# – Never mention it’s about money, even if it is.
+# • Why do you want to work here?
+# – “The applications that you build here help so many users around
+# the world, and I’d love to be a part of that.”
+# – Talk about the applications or charities that the company works
+# with. It shows that you have passion, work well in teams, and that
+# your driven.
+
+
+# – Mentioning the culture of the company would be a great answer
+# as well.
+# – Do not mention salary, benefits, or even worse… have no answer.
+# • Tell me about a tough software problem and how you solved it.
+# – “I was working on a project where I was assigned to implement
+# the Steam API into the application. Unfortunately, the API
+# wouldn’t connect properly. Using the debugger, I set break
+# points at the import and function call locations. After realizing
+# that they weren’t being hit at all, I figured it must be an issue
+# with connecting. Having tried several import variations, and
+# reading through the documentation, I decided to set up the
+# application to close when the function was hit. When I ran the
+# program the next time, it closed instantly. Realizing that the
+# function is being called, but the application isn’t running properly, I figured it had to be an import issue. It wasn’t until I tested
+# the API in a more up-to-date application that the problem was
+# due to the code being written in version 2.2, when the API
+# required version 3.6. In order to connect the API, I had to manually import the library through a mapper function that could
+# translate the code between versions. After realizing that the
+# mapper worked, I was able to implement the libraries that the
+# Steam API included in its SDK.”
+
+# Go as in depth as you can with the problem. They want to
+# know every little detail that caused the issue, how you fixed
+# the problem, and all the ideas you had in trying to solve the
+# problem. Although the preceding answer may not have made
+# much sense to you right now, it shows the problem, what I did
+# to try and find the issue, as well as how I came up with a solution
+# once I found the problem.
+
+
+
+# Whiteboarding and Technical Questions
+# This section is a list of tips that you should consider using during the third stage of the
+# interview process for both whiteboarding and technical questions:
+# • Take Your Time
+# – There’s absolutely no rush to solve a problem. Think through a
+# proper solution first before answering the question. Often, you’ll
+# think of two or three different solutions given time.
+# • Speak Out Loud
+# – Always talk through your thought process. It makes the interviewer
+# feel more comfortable so that you’re both not sitting in a quiet
+# room while you think.
+# – It shows the interviewer your ability to problem-solve.
+# – Even if you don’t give the correct answer, they can at least
+# understand where you went wrong and offer some guidance.
+# • Steps > Syntax
+# – When whiteboarding, you’ll need to write out a function or some
+# lines of code on the board in front of the interviewer. The most
+# important thing to remember is that your thought process is more
+# important than your actual code.
+# – You can have syntactical bugs on a whiteboard and still pass the
+# interview; however, having an incorrect algorithm or set of steps
+# will cause you to fail.
+
+
+
+# • Ask Questions
+# – If you’re unsure, ask questions. It’s perfectly fine to ask questions
+# when trying to solve a problem.
+# – Keep in mind the questions you ask matter though. There’s a big
+# difference in asking what a sort method does, compared to what
+# type of sort method would you like me to use.
+
+
+
+# • Algorithmic Complexity
+# – Always keep in mind the complexity of an algorithm. You’ll
+# generally be asked after you write your code if there is a way to
+# improve the performance of it even further.
+# – Know the Big O Notation category of the algorithm you just wrote.
+# – Think about what data types or collections would work best for
+# your scenario.
+# • Be Honest
+# – If you don’t know an answer, absolutely do not try and talk your
+# way through it. The interviewer during this stage is a professional
+# developer and can pick apart anything that doesn’t make sense.
+# – Being honest and saying you’re not sure but are willing to learn
+# the material will always prove to be a better method of answering
+# questions you don’t know how to solve.
+
+
+
+
+# End of Interview Questions
+# You never want to be empty handed at the end of an interview when they ask if you have
+# any questions. It’s usually good practice to take notes during an interview and write
+# down questions as you think of them. In the following, you’ll find a list of questions that
+# you should consider asking:
+# • How is the commute?
+# • Is parking free?
+# • Do you hold social events?
+# • If I wanted to further my career skills, do you guys offer any services or
+# tuition reimbursement?
+# • What kind of benefits do you offer?
+# • What is the company culture like?
+# • How many people will be working on the team with me?
+# • Will there be mentoring involved?
+# • Can you tell me more about the day-to-day responsibilities of this role?
+
+
+
+# • What do you like best about working for this company?
+# • What is the typical career path within this company for someone in
+# this role?
+# • What are the next steps in the interview process?
+# • What might I expect in a typical day?
+# • What charities does this company support?
+# • Are there any company activities, like sports teams?
+# What to Do After the Interview
+# Even if you pass the first three stages, you can still fail miserably if you don’t execute the
+# proper steps following the interview. In the following, you’ll find examples of what you
+# should do once the interview process is complete:
+
+
+
+# • Follow Up
+# – Always, always, always send an e-mail to the interviewer imme�diately, thanking them for their time. It shows respect and is a
+# courteous gesture.
+# • Critique Yourself
+# – Understand your own mistakes. Don’t take it personal; the only
+# way you can get better is by understanding and self-reflecting.
+# • Continue Building
+# – Always be working on projects and trying to improve your
+# portfolio.
+# – Stay up to date with the latest libraries, languages, and
+# technologies.
+# – Update your resume and portfolio often.
+# • Adventure Out
+# – Go out to local networking events in your area. This is where
+# you’ll meet most of your connections. It’s always easier to land a
+# job when you know someone who works in the company.
+# – Events like code alongs, or hackathons, are a great way to meet
+# other developers looking to work together.
+
+# • Rejection
+# – It happens, you won’t always get the job. If it does occur, be sure
+# to ask the interviewer in a courteous manner as to why you didn’t
+# get the job. Don’t take it personally; instead use this information
+# to become a better developer and improve.
+
+
+# Friday Project
+
+# 1
+from ShoppingCart import ShoppingCart
+ShoppingCart()
+
+# 2
+# Adding csv file to save cart items in module done successfully
+
+# Week 9 Complete........
+
+# One last week to go all done
